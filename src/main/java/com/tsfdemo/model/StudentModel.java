6031ce5628ed386aa5248de7df19eaa0c2924db9
Neha Agrawal <nehagrawal456@gmail.com>
2019-02-10 18:40:30 +0530

    Update StudentModel.java

--- src/main/java/com/tsfdemo/model/StudentModel.java
+++ src/main/java/com/tsfdemo/model/StudentModel.java
@@ -11,33 +11,44 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="StudentDetails")
-public class StudentModel {
+@Table(name="student_model")
+public class StudentModel {  
+	
+	@Id
+	@Column(name = "Id")
+	private long id; 
+	
+	@Column(name = "Name")
+	private String name;    
+	 
+	
+	public StudentModel()
+	{
+		
+	}
+	public StudentModel(long id,String name) {
+		this.id = id;
+		this.name = name;
+		
+	}
+	
+	public long getId() {    
+	    return id;    
+	}    
+	public void setId(long l) {    
+	    this.id = l;    
+	}    
+	public String getName() {    
+	    return name;    
+	}    
+	public void setName(String name) {    
+	    this.name = name;    
+	}
+	
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private int id;
-
-  @Column(name="Name")
-  private String name;
   
-  public StudentModel() {
-	  
-  }
-  
-  public StudentModel(int id,String name) {
-	  this.id = id;
-	  this.name = name;
-  }
-
-public int getId() {
-	return id;
+	
 }
-
-public void setId(int id) {
-	this.id = id;
-}
-
 public String getName() {
 	return name;
 }
